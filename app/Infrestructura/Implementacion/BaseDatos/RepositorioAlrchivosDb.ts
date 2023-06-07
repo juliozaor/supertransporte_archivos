f822cf51568adeeaf5d9072e0351842f9201285f
Julio Jimenez <zaor.julio@gmail.com>
2023-06-07 10:25:39 -0500

    Actualizaicon de archivos

--- app/Infrestructura/Implementacion/BaseDatos/RepositorioAlrchivosDb.ts
+++ app/Infrestructura/Implementacion/BaseDatos/RepositorioAlrchivosDb.ts
@@ -1,6 +1,5 @@
 
 import { RepositorioArchivos } from "../../../Dominio/Repositorios/RepositorioArchivos";
-import Drive from '@ioc:Adonis/Core/Drive';
 import { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser';
 import fs from "fs";
 import { TblArchivosTemporales } from "App/Infrestructura/datos/entidades/Archivo";
@@ -8,36 +7,54 @@ import { TblArchivosTemporales } from "App/Infrestructura/datos/entidades/Archiv
 export class RepositorioArchivosDb implements RepositorioArchivos {
 
     async crearArchivo(archivo: MultipartFileContract, datos: string): Promise<any> {
+        const basePath = './files';
+        const { idPregunta, idVigilado, temporal = true } = JSON.parse(datos);
 
-        const { idPregunta, idVigilado } = JSON.parse(datos);
+        //Parametro Temporal true
 
-        const { ruta, fecha } = this.crearCarpetaSiNoExiste();
+
+
+        const { ruta, fecha } = this.crearCarpetaSiNoExiste(basePath);
         const nombreAlmacenado = `${idPregunta}_${idVigilado}_${fecha}.${archivo.extname}`
         const nombreOriginalArchivo = archivo.clientName;
         let idTemporal;
 
-        await archivo?.moveToDisk(`${ruta}`, {
-            name: nombreAlmacenado
-        })
-
-        const archivosTemporalesBd = await TblArchivosTemporales.query().where({'art_pregunta_id':idPregunta, 'art_usuario_id':idVigilado}).first()
-        if(!archivosTemporalesBd){
+        
+        const archivosTemporalesBd = await TblArchivosTemporales.query().where({ 'art_pregunta_id': idPregunta, 'art_usuario_id': idVigilado }).first()
+        if (!archivosTemporalesBd) {
             const archivosTemporales = new TblArchivosTemporales()
-        archivosTemporales.preguntaId = idPregunta
-        archivosTemporales.usuarioId = idVigilado
-        archivosTemporales.nombreOriginal = nombreOriginalArchivo
-        archivosTemporales.nombreArchivo = nombreAlmacenado
-        archivosTemporales.rutaArchivo = ruta
-        idTemporal =  (await archivosTemporales.save()).id
-            
-        }else{
+            archivosTemporales.preguntaId = idPregunta
+            archivosTemporales.usuarioId = idVigilado
+            archivosTemporales.nombreOriginal = nombreOriginalArchivo
+            archivosTemporales.nombreArchivo = nombreAlmacenado
+            archivosTemporales.rutaArchivo = ruta
+            idTemporal = (await archivosTemporales.save()).id
+
+            await archivo?.moveToDisk(`${ruta}`, {
+                name: nombreAlmacenado
+            })
+
+        } else {
+            try {
+                fs.unlinkSync(`${basePath}${archivosTemporalesBd.rutaArchivo}/${archivosTemporalesBd.nombreArchivo}`)
+                console.log('File removed')
+              } catch(err) {
+                console.error('Something wrong happened removing the file', err)
+              }
+
             archivosTemporalesBd.nombreOriginal = nombreOriginalArchivo
             archivosTemporalesBd.nombreArchivo = nombreAlmacenado
             archivosTemporalesBd.rutaArchivo = ruta
-            idTemporal =  (await archivosTemporalesBd.save()).id
+            idTemporal = (await archivosTemporalesBd.save()).id
+
+            //actualizar el acrchivo
+            await archivo?.moveToDisk(`${ruta}`, {
+                name: nombreAlmacenado
+            })
             
+
         }
-        
+
 
 
         return { nombreAlmacenado, nombreOriginalArchivo, ruta, idTemporal }
@@ -48,12 +65,12 @@ export class RepositorioArchivosDb implements RepositorioArchivos {
         return fs.existsSync(rutaCarpeta);
     }
 
-    crearCarpetaSiNoExiste = () => {
-        const basePath = './files';
+    crearCarpetaSiNoExiste = (basePath) => {
+        
         const fechaCargue = new Date();
         const year = fechaCargue.getFullYear();
         const month = fechaCargue.getMonth() + 1;
-        const fecha = `${year}${month}`;
+        const fecha = this.format(fechaCargue);
 
         const rutaCarpeta = `${basePath}/${year}/${month}`;
         const ruta = `/${year}/${month}`
@@ -74,6 +91,25 @@ export class RepositorioArchivosDb implements RepositorioArchivos {
 
         return { ruta, fecha };
     }
+
+    format(inputDate: Date) {
+        let date, month, year, hour, minute, second;
+    
+        date = inputDate.getDate();
+        month = inputDate.getMonth() + 1;
+        year = inputDate.getFullYear();
+        hour = inputDate.getHours();
+        minute = inputDate.getMinutes();
+        second = inputDate.getSeconds();
+    
+        date = date.toString().padStart(2, '0');
+        month = month.toString().padStart(2, '0');
+        hour = hour.toString().padStart(2, '0');
+        minute = minute.toString().padStart(2, '0');
+        second = second.toString().padStart(2, '0');
+    
+          return `${year}${month}${date}${hour}${minute}${second}`;
+      }
 
 
 
