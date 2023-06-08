d4a1c070e2f4cc70048c2fdc598bf26d80996fd1
Julio Jimenez <zaor.julio@gmail.com>
2023-06-08 09:11:55 -0500

    Eliminacion de linea temporal

--- config/drive.ts
+++ config/drive.ts
@@ -7,7 +7,6 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 import { driveConfig } from '@adonisjs/core/build/config'
-import Application from '@ioc:Adonis/Core/Application'
 
 /*
 |--------------------------------------------------------------------------
