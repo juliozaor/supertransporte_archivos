685e4671ea2e95ad149adae7d84391a159523bb4
Julio Jimenez <zaor.julio@gmail.com>
2023-08-17 11:00:42 -0500

    api para la carga de evidencias

--- app/Presentacion/Archivos/ControladorArchivos.ts
+++ app/Presentacion/Archivos/ControladorArchivos.ts
@@ -10,7 +10,15 @@ export default class ControladorArchivos {
   public async guardar ({ request, response }:HttpContextContract) {
     const datos = request.all();
 
-    const archivo = request.file('archivo')
+    /* let archivo;
+    if(datos.extension){
+      archivo = request.file('archivo', {
+        extnames: ['jpg', 'jpeg', 'png', 'pdf'],
+      })
+    }else{
+      archivo = request.file('archivo')
+    } */
+   const archivo = request.file('archivo')
       if (!archivo) {
         
         return response.status(400).send({
@@ -42,6 +50,35 @@ export default class ControladorArchivos {
     public async verificarArchivos () {
     
       return this.servicio.verificarArchivos()
+      }
+
+      public async guardarEvidencias ({ request, response }:HttpContextContract) {
+        const datos = request.all();
+       
+        if(!datos.extension){
+          return "falta la extension"
+        }
+        const extension = datos.extension.replace(/\./gm, '').replace(/ /g,'');
+        const extensiones = extension.split(',');
+
+        const archivo = request.file('archivo', {
+          extnames: extensiones,
+        })
+
+          if (!archivo) {
+            
+            return response.status(400).send({
+              mensaje:'No se encontro el archivo',
+              error: 2
+            })
+          }
+    
+          if (!archivo.isValid) {
+            return response.status(400).send({mensaje:'Formato incorrecto para el archivo', error: 3})
+          }
+    
+          return this.servicio.crearArchivo(archivo, JSON.stringify(datos))
+    
       }
 
 
